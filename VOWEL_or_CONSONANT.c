f0c20476a1e0b1a810061dc680bc966b7199314a
saitanoojgrandhi <119720693+saitanoojgrandhi@users.noreply.github.com>
2022-12-23 18:12:26 +0530

    #include<stdio.h> int main() {     char n;     scanf("%c",&n);     if(n=='A'||n=='a')     {         printf("VOWEL");     }     else if(n=='E'||n=='e')     {         printf("VOWEL");     }     else if(n=='I'||n=='i')     {         printf("VOWEL");     }     else if(n=='O'||n=='o')     {         printf("VOWEL");     }     else if(n=='U'||n=='u')     {         printf("VOWEL");     }     else     {         printf("CONSONANT");     } }

---
+++ VOWEL_or_CONSONANT.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    if(n=='A'||n=='a')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='E'||n=='e')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='I'||n=='i')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='O'||n=='o')
+    {
+        printf("VOWEL");
+    }
+    else if(n=='U'||n=='u')
+    {
+        printf("VOWEL");
+    }
+    else
+    {
+        printf("CONSONANT");
+    }
+}
