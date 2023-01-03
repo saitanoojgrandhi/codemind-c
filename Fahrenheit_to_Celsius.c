2b317519531c51cd3684aca1951604a5afcf5ca1
saitanoojgrandhi <119720693+saitanoojgrandhi@users.noreply.github.com>
2023-01-03 21:27:30 +0530

    #include<stdio.h> void abba(float f) {     float c=(f-32)*5/9;     printf("%.2f",c); } int main() {     float f;     scanf("%f",&f);     abba(f); }

--- Fahrenheit_to_Celsius.c
+++ Fahrenheit_to_Celsius.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+void abba(float f)
+{
+    float c=(f-32)*5/9;
+    printf("%.2f",c);
+}
 int main()
 {
     float f;
     scanf("%f",&f);
-    float c;
-    c=(f-32)*5/9;
-    printf("%.2f",c);
+    abba(f);
 }
