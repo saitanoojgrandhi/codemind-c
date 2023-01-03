c286dd13ae09dcb8b2e29a17884fb8107686bdad
saitanoojgrandhi <119720693+saitanoojgrandhi@users.noreply.github.com>
2023-01-03 21:17:16 +0530

    #include<stdio.h> void abba(int r) {     float a=3.14*r*r;     printf("%.2f",a); } int main() {     int r;     scanf("%d",&r);     abba(r); }

--- Area_of_a_circle.c
+++ Area_of_a_circle.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+void abba(int r)
+{
+    float a=3.14*r*r;
+    printf("%.2f",a);
+}
 int main()
 {
     int r;
     scanf("%d",&r);
-    float a;
-    a=3.14*r*r;
-    printf("%.2f",a);
+    abba(r);
 }
